ba6d4a3c048f73866873ab244471291a674d4dc0
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2022-06-16 11:46:53 +0100

    polls: Update the return value of Choices model

--- polls/models.py
+++ polls/models.py
@@ -18,4 +18,4 @@ class Choices(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_answer
+        return f"{self.question} ---> {self.choice_answer}"
